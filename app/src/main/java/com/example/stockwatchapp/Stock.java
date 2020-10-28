52ea2013dba874619ad6983e1ef057c158f4cf1a
YuetaoZ <yuetao.zhu.062984@gmail.com>
2020-10-28 11:18:31 -0500

    Create Stock Class.

--- app/src/main/java/com/example/stockwatchapp/Stock.java
+++ app/src/main/java/com/example/stockwatchapp/Stock.java
@@ -1,4 +1,23 @@
 package com.example.stockwatchapp;
 
 public class Stock {
+
+    private String symbol;
+    private String name;
+    private String companyName;
+    private Double latestPrice = 0.0;
+    private Double change = 0.0;
+    private Double changePercent = 0.0;
+
+    public String getSymbol() { return symbol; }
+
+    public String getName() { return name; }
+
+    public String getCompanyName() { return companyName; }
+
+    public Double getLatestPrice() { return latestPrice; }
+
+    public Double getChange() { return change; }
+
+    public Double getChangePercent() { return changePercent; }
 }
