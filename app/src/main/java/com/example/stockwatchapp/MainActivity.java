5c687cefcf1bec078acaae98625710de3ba76389
YuetaoZ <yuetao.zhu.062984@gmail.com>
2020-10-28 10:56:08 -0500

    Update MainActivity with RecyclerView & SwipeRefreshLayout.

--- app/src/main/java/com/example/stockwatchapp/MainActivity.java
+++ app/src/main/java/com/example/stockwatchapp/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.stockwatchapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
+
+    private final List<Stock> stockList = new ArrayList<>();
+
+    private RecyclerView recyclerView;
+    private SwipeRefreshLayout swiper;
+
+    private StocksAdapter sAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        return false;
     }
 }
